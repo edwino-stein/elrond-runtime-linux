0053673f0c573b42ee358951453ba82a68acc2f5
Edwino Stein <edwino.stein@gmail.com>
2019-10-31 22:06:33 -0400

    Recatore makefiles
    Refactored the makefiles to better readability and to use the implicit 
    variables.

--- Linux.mk
+++ Linux.mk
@@ -1,26 +1,26 @@
 # General Linux build settings
-BUILD_DIR_ROOT = build
+BUILD_DIR = build
 OBJ_EXTENSION = .o
-RT_EXTENSION =
 DEPENDENCE_EXTENSION = .d
 
 # Compiler and linker settings
-COMPILER = g++
-COMPILER_COMMON_OPTIONS = -std=c++11 -Wall -MMD -c
-LINKER_COMMON_OPTIONS = -std=c++11 -Wall -rdynamic
-LIB_BUILDER = ar rvs
+CXX = g++
+CXXFLAGS = -std=c++11 -Wall -MMD -c
+LDFLAGS = -std=c++11 -Wall -rdynamic
+LDLIBS =
+AR = ar
+ARFLAGS = rvs
 
 # Params lists
-INCLUDES = $(INCLUDE_DIR_ROOT)
+INCLUDES = $(INCLUDE_DIR)
 MACROS = GENERIC_STD_PLATFORM LINUX_PLATFORM
 STATIC_LIBRARIES =
 DYNAMIC_LIBRARIES = dl pthread m
-RAW_DLIBRARIES =
 
-# Elrond Common library setup
+# Elrond COMMON library setup
 COMMON_DIR = elrond
-COMMON_NAME_LIB = lib-elrond-common
-COMMON_LIB = build/$(COMMON_NAME_LIB).a
+COMMON_NAME_LIB = libelrond
+COMMON_NONPIC_LIB = build/$(COMMON_NAME_LIB).a
 
 # if has a dev common version in the project, use it
 ifeq ($(shell test -L $(COMMON_DIR)-dev -o -d $(COMMON_DIR)-dev; echo $$?), 0)
@@ -28,49 +28,50 @@ ifeq ($(shell test -L $(COMMON_DIR)-dev -o -d $(COMMON_DIR)-dev; echo $$?), 0)
 endif
 INCLUDES += $(COMMON_DIR)/include
 
-#Define build dir as target for all src files
-OBJS_FILES = $(subst $(SRC_DIR_ROOT)/,$(BUILD_DIR_ROOT)/,$(SRC_FILES))
-
-# Set NOPIC objects
+# Define object files
+OBJS_FILES = $(subst $(SRC_DIR)/,$(BUILD_DIR)/,$(SRC_FILES))
 OBJS := $(addsuffix $(OBJ_EXTENSION), $(OBJS_FILES))
+
+# Define dependencies files
 DEPS = $(OBJS:$(OBJ_EXTENSION)=$(DEPENDENCE_EXTENSION))
 
 # Add includes and macros to compiler options
-COMPILER_COMMON_OPTIONS += $(addprefix -I, $(INCLUDES))
-COMPILER_COMMON_OPTIONS += $(addprefix -D, $(MACROS))
+CXXFLAGS += $(addprefix -I, $(INCLUDES))
+CXXFLAGS += $(addprefix -D, $(MACROS))
 
 # Set Dynamic library to linkage
-RAW_DLIBRARIES += $(addprefix -l, $(DYNAMIC_LIBRARIES))
+LDLIBS += $(addprefix -l, $(DYNAMIC_LIBRARIES))
+
+.PHONY: $(PROJECT_NAME) clean-build clean-common clean-build-all
 
 # ************************** BUILD RULES **************************
 
-.PHONY: $(PROJECT_NAME)$(RT_EXTENSION) clean-build clean-build-all clean-common run
-
 # Runtime object builder
-$(PROJECT_NAME)$(RT_EXTENSION): $(BUILD_DIR_ROOT)/$(PROJECT_NAME)$(RT_EXTENSION)
-$(BUILD_DIR_ROOT)/$(PROJECT_NAME)$(RT_EXTENSION): $(BUILD_DIR_ROOT)/$(PROJECT_NAME).a $(COMMON_DIR)/$(COMMON_LIB)
+$(PROJECT_NAME): $(BUILD_DIR)/$(PROJECT_NAME)
+$(BUILD_DIR)/$(PROJECT_NAME): $(BUILD_DIR)/$(PROJECT_NAME).a $(COMMON_DIR)/$(COMMON_NONPIC_LIB)
 	@mkdir -p $(@D)
-	$(COMPILER) $(LINKER_COMMON_OPTIONS) $^ $(STATIC_LIBRARIES) -o $@ $(RAW_DLIBRARIES)
+	$(CXX) $(LDFLAGS) $^ -o $@ $(LDLIBS)
 
-$(BUILD_DIR_ROOT)/$(PROJECT_NAME).a: $(OBJS)
+# Static library builder
+$(BUILD_DIR)/$(PROJECT_NAME).a: $(OBJS)
 	@mkdir -p $(@D)
-	@$(LIB_BUILDER) $@ $?
+	@$(AR) $(ARFLAGS) $@ $?
 
 # Objects builder
-$(BUILD_DIR_ROOT)/%$(OBJ_EXTENSION): $(SRC_DIR_ROOT)/%
+$(BUILD_DIR)/%.cpp$(OBJ_EXTENSION): $(SRC_DIR)/%.cpp
 	@mkdir -p $(@D)
-	$(COMPILER) $(COMPILER_COMMON_OPTIONS) $< -o $@
+	$(CXX) $(CXXFLAGS) $< -o $@
 
 # Elrond common library builder
-$(COMMON_DIR)/$(COMMON_LIB):
-	cd $(COMMON_DIR) && $(MAKE) $(notdir $@)
+$(COMMON_DIR)/$(COMMON_NONPIC_LIB):
+	@cd $(COMMON_DIR) && $(MAKE) $(notdir $@)
 
 # Cleaning rules
-clean-common:
-	cd $(COMMON_DIR) && $(MAKE) clean
-
 clean-build:
-	rm -rf $(BUILD_DIR_ROOT)
+	rm -rf $(BUILD_DIR)
+
+clean-common:
+	@cd $(COMMON_DIR) && $(MAKE) clean
 
 clean-build-all: clean-build clean-common
 
